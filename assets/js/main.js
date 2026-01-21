7b6dfae4e059d99dec73cdb3285ea61b15b55ebc
csumie <csurdanivia@gmail.com>
2026-01-20 20:49:12 -0500

    HTML and JS update

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ function setDate() {
   const now = new Date();
   const seconds = now.getSeconds();
   const secondsDegrees = ((seconds / 60) * 360;
-
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
   console.log(seconds);
 }
 
