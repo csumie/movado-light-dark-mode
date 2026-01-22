a201cf214cb9a32b3712eda4153bf46a97d72cf7
csumie <csurdanivia@gmail.com>
2026-01-21 19:32:15 -0500

    JS update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,9 @@
 const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
 
 function setDate() {
   const now = new Date();
+
   const seconds = now.getSeconds();
   const secondsDegrees = ((seconds / 60) * 360);
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
